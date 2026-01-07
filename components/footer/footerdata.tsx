9d1e08b0804981a4579af3c0d003a50c850a3ab9
sumit kumar <sumit@eazotel.com>
2026-01-07 17:57:45 +0530

    fix: update footer contact titles for clarity

--- components/footer/footerdata.tsx
+++ components/footer/footerdata.tsx
@@ -37,13 +37,13 @@ export const footerData: FooterData = {
           href: contact.addressLink,
         },
         {
-          title: "Hindi: ",
+          title: "Call: ",
           icon: <FillCallIcon />,
           label: contact.phone[0],
           href: "tel:" + contact.phone[0],
         },
         {
-          title: "English: ",
+          title: "Call: ",
           icon: <FillCallIcon />,
           label: contact.phone[1],
           href: "tel:" + contact.phone[1],
