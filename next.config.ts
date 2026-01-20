df1c6088566f4b2b4bd85c98ecaffab9a5642d98
Personal <sk7627577@gmail.com>
2026-01-20 18:48:55 +0530

    add Google Tag Manager integration to RootLayout

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   trailingSlash: true,
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
+  // output: "export",
+  // images: {
+  //   unoptimized: true,
+  // },
 };
 
 export default nextConfig;
